1a272d35cfc740a299db6aec1790a94f89ab02c7
Andrey <Andrey@MSI>
2015-08-12 20:12:46 +0300

    0

---
+++ src/custom/Main.java
@@ -0,0 +1,11 @@
+package custom;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Java!!!!");
+
+	}
+
+}
---
+++ src/net/mindview/util/OSExecuteException.java
@@ -0,0 +1,9 @@
+//: net/mindview/util/OSExecuteException.java
+package net.mindview.util;
+
+@SuppressWarnings("serial")
+public class OSExecuteException extends RuntimeException {
+	public OSExecuteException(String why) {
+		super(why);
+	}
+} /// :~
