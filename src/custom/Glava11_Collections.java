93aa4353b374d2a7a68ffab0df2fe8b11972f54c
Andrey <Andrey@MSI>
2015-09-06 21:25:17 +0300

    1

--- src/custom/Glava11_Collections.java
+++ src/custom/Glava11_Collections.java
@@ -6,10 +6,9 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class Main {
+public class Glava11_Collections {
 
 	public static void main(String[] args) {
-
 		Collection<Integer> collection = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
 
 		Integer[] moreInts = { 6, 7, 8, 9, 10 };
