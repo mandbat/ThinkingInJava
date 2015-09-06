d4dfb0ef1b62ef4a4a22e3aaed93c20f51280ee4
Andrey <Andrey@MSI>
2015-09-06 21:58:51 +0300

    1

--- src/custom/С11_Collections.java
+++ src/custom/С11_Collections.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class Glava11_Collections {
+public class С11_Collections {
 
 	public static void main(String[] args) {
 		Collection<Integer> collection = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
